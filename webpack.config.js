4663fc8c10692ec1e006a2ffb9cf820d7fcc1969
Ali Al Dallal <ali@alicoding.com>
2015-06-15 13:00:40 -0400

    Add React Hot Loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,35 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-  entry: {
-    app: './index.jsx'
-  },
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server',
+    './index.jsx' // Your appʼs entry point
+  ],
   devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
   output: {
-    path: __dirname + '/public',
-    filename: '[name].bundle.js'
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   },
   module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loaders: ['jsx-loader', 'babel?optional[]=runtime&stage=0'],
-      }
-    ]
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loaders: ['react-hot', 'babel'],
+    }]
   },
   devServer: {
       contentBase: "./public",
       noInfo: true, //  --no-info option
       hot: true,
       inline: true
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 };
