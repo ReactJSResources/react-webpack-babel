edb746ec348aa876ea85fc76fa4fcc391e6d9f81
Ali Al Dallal <ali@alicoding.com>
2016-03-03 09:35:12 -0500

    Update space to tabs

--- webpack.config.js
+++ webpack.config.js
@@ -2,69 +2,68 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-    entry: [
-    'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
-    'webpack/hot/only-dev-server',
-    './index.jsx' // Your appʼs entry point
-  ],
-    devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loaders: ['react-hot', 'babel'],
-            },
+	entry: [
+		'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+		'webpack/hot/only-dev-server',
+		'./index.jsx' // Your appʼs entry point
+	],
+	devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loaders: ['react-hot', 'babel'],
+			},
 
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "file"
-            },
-            {
-                test: /\.(woff|woff2)$/,
-                loader: "url?prefix=font/&limit=5000"
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&mimetype=application/octet-stream"
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&mimetype=image/svg+xml"
-            },
-            {
-                test: /\.gif/,
-                loader: "url-loader?limit=10000&mimetype=image/gif"
-            },
-            {
-                test: /\.jpg/,
-                loader: "url-loader?limit=10000&mimetype=image/jpg"
-            },
-            {
-                test: /\.png/,
-                loader: "url-loader?limit=10000&mimetype=image/png"
-            }
-
-             ]
-    },
-    devServer: {
-        contentBase: "./public",
-        noInfo: true, //  --no-info option
-        hot: true,
-        inline: true
-    },
-    plugins: [
-      new webpack.NoErrorsPlugin()
-    ]
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader'
+			},
+			{
+				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "file"
+			},
+			{
+				test: /\.(woff|woff2)$/,
+				loader: "url?prefix=font/&limit=5000"
+			},
+			{
+				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "url?limit=10000&mimetype=application/octet-stream"
+			},
+			{
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "url?limit=10000&mimetype=image/svg+xml"
+			},
+			{
+				test: /\.gif/,
+				loader: "url-loader?limit=10000&mimetype=image/gif"
+			},
+			{
+				test: /\.jpg/,
+				loader: "url-loader?limit=10000&mimetype=image/jpg"
+			},
+			{
+				test: /\.png/,
+				loader: "url-loader?limit=10000&mimetype=image/png"
+			}
+		]
+	},
+	devServer: {
+		contentBase: "./public",
+			noInfo: true, //  --no-info option
+			hot: true,
+			inline: true
+		},
+	plugins: [
+		new webpack.NoErrorsPlugin()
+	]
 };
