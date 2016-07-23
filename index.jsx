bd5cfa4bd3a97c4022514770d963242a76a78928
Radek Novak <radek.novak.cze@gmail.com>
2016-07-24 00:09:59 +0200

    change attribute class to className

--- index.jsx
+++ index.jsx
@@ -8,7 +8,7 @@ export class App extends React.Component {
 			<div>
 				<h1>Jumbotron</h1>
 				<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-				<p><a class="btn btn-primary btn-lg">Learn more</a></p>
+				<p><a className="btn btn-primary btn-lg">Learn more</a></p>
 			</div>
 		);
 	}
