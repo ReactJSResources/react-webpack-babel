910fdc37397d08446809ed74316cdc219157da0e
Dmitry Pavlov <dmitrypavlov.design@gmail.com>
2016-08-30 16:55:21 +0300

    exclude node_modules/bower_components folders

--- webpack.loaders.js
+++ webpack.loaders.js
@@ -15,30 +15,37 @@ module.exports = [
 	},
 	{
 		test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+		exclude: /(node_modules|bower_components)/,
 		loader: "file"
 	},
 	{
 		test: /\.(woff|woff2)$/,
+		exclude: /(node_modules|bower_components)/,
 		loader: "url?prefix=font/&limit=5000"
 	},
 	{
 		test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+		exclude: /(node_modules|bower_components)/,
 		loader: "url?limit=10000&mimetype=application/octet-stream"
 	},
 	{
 		test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+		exclude: /(node_modules|bower_components)/,
 		loader: "url?limit=10000&mimetype=image/svg+xml"
 	},
 	{
 		test: /\.gif/,
+		exclude: /(node_modules|bower_components)/,
 		loader: "url-loader?limit=10000&mimetype=image/gif"
 	},
 	{
 		test: /\.jpg/,
+		exclude: /(node_modules|bower_components)/,
 		loader: "url-loader?limit=10000&mimetype=image/jpg"
 	},
 	{
 		test: /\.png/,
+		exclude: /(node_modules|bower_components)/,
 		loader: "url-loader?limit=10000&mimetype=image/png"
 	}
 ];
