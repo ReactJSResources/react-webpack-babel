dd18cf3c9a0b46245534abbaaa1a65555ac3aaa7
IanChar <ianchar4@gmail.com>
2016-10-04 12:44:38 -0600

    Created click handler for changing color in cell.

--- src/components/cell.jsx
+++ src/components/cell.jsx
@@ -2,14 +2,31 @@ import styles from '.././index.scss';
 import React from 'react';
 
 export default class Cell extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+          color: 'white'
+      }
+      this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    if (this.state.color === 'white') {
+        this.setState({color: 'black'});
+    } else {
+        this.setState({color: 'white'});
+    }
+  }
+
   render() {
+    const color_style = {color: this.state.color};
     return (
       <div>
-        <div className={styles.cell}></div>
+        <div onClick={this.handleClick}
+             style={color_style}
+             className={styles.cell}>
+        </div>
       </div>
     )
-  }
-  _handleClick() {
-    console.log("clicked");
   }
 }
