8f46602e190d1d640067aff5f193575697e83549
IanChar <ianchar4@gmail.com>
2016-10-05 21:03:01 -0600

    Added basic worker script,

---
+++ worker/worker.js
@@ -0,0 +1,9 @@
+// The frequency in seconds in which to make some move on the grid.
+FREQUENCE_S = 15;
+
+// This function should be replaced by some script that makes updates Firebase.
+function stubFunction() {
+    console.log("Triggered");
+}
+
+setInterval(stubFunction, frequency_s * 1000);
