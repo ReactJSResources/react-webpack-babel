ebc8d0944549005a11ada18be6c68fce94f861b3
Artur Meletyan <artm95@gmail.com>
2016-10-06 07:16:25 -0600

    added function to toggle colors in firebase

--- worker/worker.js
+++ worker/worker.js
@@ -3,7 +3,59 @@ FREQUENCE_S = 15;
 
 // This function should be replaced by some script that makes updates Firebase.
 function stubFunction() {
-    console.log("Triggered");
+	console.log("Triggered");
 }
+
+
+
+function changeColor() {
+
+	var dimensions = {};
+
+	// connect to grid and count rows
+	var rowCountRef = firebase.database().ref('grid0/');
+	rowCountRef.once("value", function(snapshot) {
+	
+		dimensions.rows = snapshot.numChildren();
+
+		// decide on a random row
+		var randomRow = 'r' + Math.floor(Math.random() * dimensions.rows) + '/';
+
+		// connect to row and count columns
+		var colCountRef = firebase.database().ref('grid0/' + randomRow);
+		colCountRef.once("value", function(snapshot) {
+
+			dimensions.columns = snapshot.numChildren();
+
+			// decide on a random column
+			var randomColumn = 'c' + Math.floor(Math.random() * dimensions.columns) + '/';
+
+			// connect to column and choose random cell
+			var cellRef = firebase.database().ref('grid0/' + randomRow + randomColumn);
+
+			// change cell color
+			cellRef.transaction(function(currentData) {
+
+				if (currentData === "#000000") {
+				
+					// if white, then black
+					return "#ffffff";
+				
+				} else {
+				
+					// if black, then white
+					return "#000000";
+
+				}
+
+			});
+
+		});
+
+	});
+
+};
+
+
 
 setInterval(stubFunction, frequency_s * 1000);
