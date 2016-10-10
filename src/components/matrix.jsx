27c1ac7988bb3b9430972547f5d4821dd3d9274e
Zach Lamb <zala3440@colorado.edu>
2016-10-09 21:41:20 -0600

    abstracted the data to data.jsx for one source of truth

--- src/components/matrix.jsx
+++ src/components/matrix.jsx
@@ -2,18 +2,13 @@ import React from 'react';
 import Column from './row.jsx';
 import styles from '.././index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {matrixID} from './data.jsx';
 export default class Matrix extends React.Component {
   render(){
-    return(
-      <div className={styles.matrix}>
-        <Column/>
-        <Column/>
-        <Column/>
-        <Column/>
-        <Column/>
-        <Column/>
-      </div>
-    )
+    var matrix = []
+    for(var i=0; i<matrixID[0];i++){
+        matrix.push(<Column/>)
+    }
+  return(<div className={styles.matrix}>{matrix}</div>)
   }
 }
--- src/components/row.jsx
+++ src/components/row.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import Cell from './cell.jsx';
 import styles from '.././index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {matrixID} from './data.jsx'
 
 export default class Column extends React.Component {
   render(){
+    var matrix = []
+    for(var i=0;i<matrixID[1];i++){
+      matrix.push(<Cell/>)
+    }
     return(
-      <div className={styles.matrixColumn}>
-        <Cell/>
-        <Cell/>
-        <Cell/>
-        <Cell/>
-        <Cell/>
-        <Cell/>
-      </div>
+      <div className={styles.matrixColumn}>{matrix}</div>
     )
   }
 }
