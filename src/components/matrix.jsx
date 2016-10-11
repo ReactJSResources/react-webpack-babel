db6de471d59eb103be24ff9aebdd14d236fddade
Zach Lamb <zala3440@colorado.edu>
2016-10-10 18:13:35 -0600

    moved code from row component into matrix

--- src/components/matrix.jsx
+++ src/components/matrix.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import Column from './row.jsx';
+import Cell from './cell.jsx';
 import styles from '.././index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {matrixID} from './data.jsx';
 export default class Matrix extends React.Component {
   render(){
-    var matrix = []
+    let matrix = []
     for(var i=0; i<matrixID[0];i++){
-        matrix.push(<Column column={i}/>)
+      let column = []
+      for(var j=0;j<matrixID[1];j++){
+        column.push(<Cell id={"R"+i + "C" + j}/>)
+      }
+      matrix.push(<div className={styles.matrixColumn}>{column}</div>)
     }
   return(<div className={styles.matrix}>{matrix}</div>)
   }
