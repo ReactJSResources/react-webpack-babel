be3996292c8cdb022255e0ce4f1b35454e2a8290
vydimitrov <v.y.dimitrov@gmail.com>
2016-10-15 11:40:34 +0200

    Added autoprefixer and fixed issue with ExtractTextPlugin
    ExtractTextPlugin, when multiple loaders provided they should be in a single string separate with " ! "

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -6,18 +6,27 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var WebpackCleanupPlugin = require('webpack-cleanup-plugin');
 
+const AUTOPREFIXER_BROWSERS = [
+  'Android 2.3',
+  'Android >= 4',
+  'Chrome >= 35',
+  'Firefox >= 31',
+  'Explorer >= 9',
+  'iOS >= 7',
+  'Opera >= 12',
+  'Safari >= 7.1',
+];
+
 // local css modules
 loaders.push({
 	test: /[\/\\]src[\/\\].*\.css/,
-	exclude: /(node_modules|bower_components|public)/,
 	loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]')
 });
 
 // local scss modules
 loaders.push({
 	test: /[\/\\]src[\/\\].*\.scss/,
-	exclude: /(node_modules|bower_components|public)/,
-	loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]', 'sass')
+	loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass')
 });
 // global css files
 loaders.push({
@@ -60,8 +69,11 @@ module.exports = {
 		}),
 		new HtmlWebpackPlugin({
 			template: './src/template.html',
-			title: 'Webpack App'
+			title: 'To-Do List'
 		}),
 		new webpack.optimize.DedupePlugin()
-	]
+	],
+	postcss: [
+    	require('autoprefixer')({ browsers: AUTOPREFIXER_BROWSERS })
+  	]
 };
