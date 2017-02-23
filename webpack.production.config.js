3f3217ee719e88136c4bcaf23c53580ed36f77f1
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-02-23 08:27:06 -0500

    updated dependencies

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,9 +5,9 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var WebpackCleanupPlugin = require('webpack-cleanup-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-loaders.push({ 
-	test: /\.scss$/, 
-	loader: ExtractTextPlugin.extract('style', 'css?sourceMap&localIdentName=[local]___[hash:base64:5]!sass?outputStyle=expanded'),
+loaders.push({
+	test: /\.scss$/,
+	loader: ExtractTextPlugin.extract({fallback: 'style-loader', use : 'css-loader?sourceMap&localIdentName=[local]___[hash:base64:5]!sass-loader?outputStyle=expanded'}),
 	exclude: ['node_modules']
 });
 
@@ -22,7 +22,7 @@ module.exports = {
 		filename: '[chunkhash].js'
 	},
 	resolve: {
-		extensions: ['', '.js', '.jsx']
+		extensions: ['.js', '.jsx']
 	},
 	module: {
 		loaders
@@ -42,10 +42,10 @@ module.exports = {
 				drop_debugger: true
 			}
 		}),
-		new webpack.optimize.OccurenceOrderPlugin(),
-		new webpack.optimize.DedupePlugin(),
-	    new ExtractTextPlugin("style.css", {
-		      allChunks: true
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new ExtractTextPlugin({
+				filename: 'style.css',
+				allChunks: true
 		}),
 		new HtmlWebpackPlugin({
 			template: './src/template.html',
