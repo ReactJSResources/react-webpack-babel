ef5cbed501cdd4d246c60a8c4b50d513e9c0c21a
erfanio <dev@erfan.io>
2017-04-04 21:57:00 +1000

    fix inconsistent indentation

--- postcss.config.js
+++ postcss.config.js
@@ -10,7 +10,7 @@ const AUTOPREFIXER_BROWSERS = [
 ];
 
 module.exports = {
-	plugins: [
-		require('autoprefixer')({ browsers: AUTOPREFIXER_BROWSERS })
-	]
+  plugins: [
+    require('autoprefixer')({ browsers: AUTOPREFIXER_BROWSERS })
+  ]
 }
