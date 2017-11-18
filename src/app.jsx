9b418a0ed4e9ac5376849ca95ed7f51fe76a4295
Peony Gerochi <itsduc@gmail.com>
2017-11-18 15:16:37 -0500

    refactor(react-router): refactor of react router.

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Navigation from './components/Navigation';
+import 'styles/index.scss';
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div className='App'>
 				<Navigation/>
-				<div className='Page'>
-					{ this.props.children }
+				<div>
+					<h1>It Works!</h1>
+					<p>This React project just works including <span className="redBg">module</span> local styles.</p>
+					<p>Enjoy!</p>
 				</div>
 			</div>
 		)
