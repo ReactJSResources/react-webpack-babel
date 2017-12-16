8f54cce6504fd7bf385434324eced2d732a61a04
Peony Gerochi <peony.gero>
2017-12-15 22:49:36 -0500

    fix (webpack): changed url-loader to file-loaders in webpack.loaders.js, added a sample image under About page.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import dog from '../images/dog.png';
 
 const About = () => (
   <div>
     Hello
+		<img src={dog} className="small-img"/>
   </div>
 );
 
--- webpack.loaders.js
+++ webpack.loaders.js
@@ -17,31 +17,31 @@ module.exports = [
   {
     test: /\.(woff|woff2)$/,
     exclude: /(node_modules|bower_components)/,
-    loader: "url-loader?prefix=font/&limit=5000"
+    loader: "file-loader?prefix=font/&limit=5000"
   },
   {
     test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
     exclude: /(node_modules|bower_components)/,
-    loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+    loader: "file-loader?limit=10000&mimetype=application/octet-stream"
   },
   {
     test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
     exclude: /(node_modules|bower_components)/,
-    loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+    loader: "file-loader?limit=10000&mimetype=image/svg+xml"
   },
   {
     test: /\.gif/,
     exclude: /(node_modules|bower_components)/,
-    loader: "url-loader?limit=10000&mimetype=image/gif"
+    loader: "file-loader?limit=10000&mimetype=image/gif"
   },
   {
     test: /\.jpg/,
     exclude: /(node_modules|bower_components)/,
-    loader: "url-loader?limit=10000&mimetype=image/jpg"
+    loader: "file-loader?limit=10000&mimetype=image/jpg"
   },
   {
     test: /\.png/,
     exclude: /(node_modules|bower_components)/,
-    loader: "url-loader?limit=10000&mimetype=image/png"
+    loader: "file-loader?limit=10000&mimetype=image/png"
   }
 ];
