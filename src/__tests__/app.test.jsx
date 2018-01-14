fba918e3e6a7bf0d55549024dbcd42c88457aedf
Emily Morehouse <emily@cuttlesoft.com>
2018-01-14 13:49:37 -0700

    The included test did not run properly, as it did not render as a component since it was lower case. Swapped out testing App for Router, as this renders the app.

--- src/__tests__/app.test.jsx
+++ src/__tests__/app.test.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import app from 'app';
+import Routes from '../routes';
 
 configure({ adapter: new Adapter() });
 
 describe('app', () => {
   it('renders without crashing', () => {
-    mount(<app />);
+    mount(<Routes />);
   });
 });
