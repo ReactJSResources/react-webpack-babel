8a6e8ca74fd6e0a00ee840245da75d55a1d8114e
Leonardo Gentile <leonard.gentile@gmail.com>
2018-03-04 18:39:35 +0100

    Comments

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,8 @@ const PORT = process.env.PORT || "8888";
 module.exports = {
   entry: [
     // POLYFILL: Set up an ES6-ish environment
-    // Either the entire babel-polyfill
-    // Or fine-tune es6 features needed (included into babel-polyfill)
-    // Example:
     // 'babel-polyfill',  // The entire babel-polyfill
+    // Or pick es6 features needed (included into babel-polyfill)
     'core-js/fn/promise',
     'core-js/es6/object',
     'core-js/es6/array',
